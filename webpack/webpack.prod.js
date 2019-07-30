8ad0dde03432e84cc7358f7edc044894abb4302b
一秋 <37320849+supergczh@users.noreply.github.com>
2019-07-30 22:39:37 +0800

    Update webpack.prod.js

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -132,7 +132,7 @@ module.exports = merge(bash, {
       cacheGroups: {
         vendor: {
           // 抽离第三方插件
-          test: /react/, // 指定是node_modules下的第三方包
+          test: /vue/, // 指定是node_modules下的第三方包
           chunks: "initial",
           name: "vendor", // 打包后的文件名，任意命名
           // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
